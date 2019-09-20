03bf5a8e9728ebfff8abdd42d1c4a6b984b7b7c6
Emil Priver <emilpriver1@gmail.com>
2019-09-20 22:33:05 +0200

    Delete start2pay.js

--- start2pay.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
- 
-	var checkout_form = $( 'form.woocommerce-checkout' );
-	checkout_form.on( 'checkout_place_order', function() {
-        //console.log('hej')
-    } );
- 
-});
